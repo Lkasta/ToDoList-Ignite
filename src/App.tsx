7c753ead3710039dad411a68841c980fd9c69df6
Lkasta <lkastabackup@gmail.com>
2025-08-14 22:41:15 -0300

    feat: adjust css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import styles from './App.module.css'
-import './global.css'
+import styles from "./App.module.css";
+import "./global.css";
 
-import { CreateTask } from './components/CreateTask'
-import { EmptyList } from './components/EmptyList'
-import { Header } from './components/Header'
-import { Task } from './components/Task'
-import { TasksMain } from './components/TasksMain'
-import { useState } from 'react'
+import { CreateTask } from "./components/CreateTask";
+import { EmptyList } from "./components/EmptyList";
+import { Header } from "./components/Header";
+import { Task } from "./components/Task";
+import { TasksMain } from "./components/TasksMain";
+import { useState } from "react";
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 interface taskProps {
   id: string;
@@ -17,63 +17,47 @@ interface taskProps {
 }
 
 function App() {
-
   const [taskList, setTaskList] = useState<taskProps[]>([
     {
-      id: '1',
+      id: "1",
       isComplete: false,
-      content: 'Fazer uma bela lista de compras'
+      content: "Fazer uma bela lista de compras",
     },
-    {
-      id: '2',
-      isComplete: true,
-      content: 'Eu não estou em perigo, Skyler. Eu sou o perigo. Acha que sou eu quem vai atender a porta e levar um tiro? Não. Eu sou o cara que bate na porta....'
-    },
-    {
-      id: '3',
-      isComplete: false,
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident animi, repudiandae quia, laboriosam ducimus enim rerum nobis corporis sunt tenetur dolorum sed repellat ipsum, quod officiis voluptate odit incidunt id'
-    },
-    {
-      id: '4',
-      isComplete: true,
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident animi, '
-    }
-  ])
-
-  const [tasks, setTasks] = useState({ id: '', isComplete: true, content: '' })
+  ]);
 
   function hasTaskInList() {
-    let has
-    taskList.length != 0 ? has = true : has = false
-    return has
+    let has;
+    taskList.length != 0 ? (has = true) : (has = false);
+    return has;
   }
 
   function countTasksComplete() {
-    let sum: number = 0;
-    taskList.map(task => {
-      task.isComplete ? sum += 1 : sum = sum
-    })
-    return sum
+    let sum = 0;
+    taskList.map((task) => {
+      task.isComplete ? (sum += 1) : sum;
+    });
+    return sum;
   }
 
   function createTask(content: string) {
-    event?.preventDefault()
-    const newTask: taskProps = { id: uuidv4(), isComplete: false, content: content }
-    setTasks(newTask)
-    setTaskList([...taskList, newTask])
+    const newTask: taskProps = {
+      id: uuidv4(),
+      isComplete: false,
+      content: content,
+    };
+    setTaskList([...taskList, newTask]);
   }
 
   function handleDeleteTask(id: string) {
-    const tasksWhithoutDeleteOne = taskList.filter(task => {
+    const tasksWhithoutDeleteOne = taskList.filter((task) => {
       return task.id !== id;
-    })
-    setTaskList(tasksWhithoutDeleteOne)
+    });
+    setTaskList(tasksWhithoutDeleteOne);
   }
 
   function handleCompleteTask(id: string) {
-    setTaskList(prevList => {
-      return prevList.map(task => {
+    setTaskList((prevList) => {
+      return prevList.map((task) => {
         if (task.id === id) {
           if (task.isComplete == true) {
             return { ...task, isComplete: false };
@@ -87,7 +71,7 @@ function App() {
   }
 
   function renderTasks() {
-    return taskList.map(task => {
+    return taskList.map((task) => {
       return (
         <Task
           key={task.id}
@@ -97,8 +81,8 @@ function App() {
           handleDeleteTask={handleDeleteTask}
           handleCompleteTask={handleCompleteTask}
         />
-      )
-    })
+      );
+    });
   }
 
   return (
@@ -106,11 +90,14 @@ function App() {
       <Header />
       <div className={styles.container}>
         <CreateTask createTask={createTask} />
-        <TasksMain createdTasks={taskList.length} completedTasks={countTasksComplete()} />
+        <TasksMain
+          createdTasks={taskList.length}
+          completedTasks={countTasksComplete()}
+        />
         {hasTaskInList() ? renderTasks() : <EmptyList />}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,6 +1,6 @@
-import styles from './Task.module.css'
+import styles from "./Task.module.css";
 
-import { Trash } from '@phosphor-icons/react'
+import { Trash } from "@phosphor-icons/react";
 
 export interface TaskType {
   id: string;
@@ -11,17 +11,22 @@ interface TasksProps {
   task: TaskType;
   content: string;
   isComplete: boolean;
-  handleDeleteTask?: (id: string) => void ;
+  handleDeleteTask?: (id: string) => void;
   handleCompleteTask?: (id: string) => void;
 }
 
-export function Task({ task, isComplete, handleDeleteTask, handleCompleteTask}: TasksProps) {
+export function Task({
+  task,
+  isComplete,
+  handleDeleteTask,
+  handleCompleteTask,
+}: TasksProps) {
   function handleDeleteTasks() {
     if (handleDeleteTask) {
       handleDeleteTask(task.id);
     }
   }
-  
+
   function handleCompleteTasks() {
     if (handleCompleteTask) {
       handleCompleteTask(task.id);
@@ -30,11 +35,16 @@ export function Task({ task, isComplete, handleDeleteTask, handleCompleteTask}: 
 
   return (
     <div className={styles.task}>
-      <strong className={isComplete ? styles.circleComplete : styles.circle} onClick={handleCompleteTasks}/>
-      <p className={isComplete ? styles.contentComplete : styles.content}>{task.content}</p>
+      <strong
+        className={isComplete ? styles.circleComplete : styles.circle}
+        onClick={handleCompleteTasks}
+      />
+      <p className={isComplete ? styles.contentComplete : styles.content}>
+        {task.content}
+      </p>
       <span className={styles.trash}>
-        <Trash size={20} onClick={handleDeleteTasks}/>
+        <Trash size={20} onClick={handleDeleteTasks} />
       </span>
     </div>
-  )
+  );
 }
